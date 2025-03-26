8b9928386f8b3b2e3e2754384a718ed9d46cbd73
michael-chxn <michaelchxx@gmail.com>
2025-03-26 19:04:32 -0400

    2fa 2

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def post_story():
     except Exception as e:
         print(f"Error: {e}")
 
-schedule.every().day.at("23:04").do(post_story)
+schedule.every().day.at("23:06").do(post_story)
 
 print("Script running... Posting stories daily.")
 while True:
