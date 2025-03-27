aa16bf3442c745406357d2046daf1c512d33c3f1
michael-chxn <michaelchxx@gmail.com>
2025-03-26 22:15:07 -0400

    pics v2

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ from instagrapi import Client
 USERNAME = os.getenv("IG_USERNAME")
 PASSWORD = os.getenv("IG_PASSWORD")
 VIDEO_FOLDER = "videos/"
+PIC_FOLDER = "pics/"
 
 def post_story():
     try:
@@ -65,12 +66,20 @@ def post_story():
         cl.video_upload_to_story(video_path)
         print(f"Uploaded {video_path} to IG Story!")
 
+        pics = [f for f in os.listdir(PIC_FOLDER) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+        if pics:
+            pic_path = os.path.join(PIC_FOLDER, random.choice(pics))
+            cl.photo_upload_to_story(pic_path)
+            print(f"Uploaded image: {pic_path}")
+        else:
+            print("No images found in the 'pics' folder.")
+
     except Exception as e:
         print(f"Error: {e}")
 
-schedule.every().day.at("02:15").do(post_story)
+schedule.every().day.at("02:17").do(post_story)
 
-print("Script running... Posting stories daily 10:15 PM.")
+print("Script running... Posting stories daily 10:17 PM.")
 while True:
     schedule.run_pending()
     time.sleep(60)
