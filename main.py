d031aacdb0e74224a814905e85a5b8e3f0e762a0
michael-chxn <michaelchxx@gmail.com>
2025-03-26 22:14:05 -0400

    missed test time

--- main.py
+++ main.py
@@ -68,9 +68,9 @@ def post_story():
     except Exception as e:
         print(f"Error: {e}")
 
-schedule.every().day.at("02:13").do(post_story)
+schedule.every().day.at("02:15").do(post_story)
 
-print("Script running... Posting stories daily 10:13 PM.")
+print("Script running... Posting stories daily 10:15 PM.")
 while True:
     schedule.run_pending()
     time.sleep(60)
